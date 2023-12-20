ab6e35e05488839b75afc80da64cb219562b9079
IhorPozhematkin <132217496+IhorPozhematkin@users.noreply.github.com>
2023-12-20 21:12:07 +0200

    goit-react-hw-07-phonebook 20.12-3

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,15 +2,10 @@ import { ContactItem } from 'components/ContactItem/ContactItem';
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchContacts } from './../../redux/operations';
-import {
-  selectContacts,
-  selectError,
-  selectFilteredContacts,
-} from './../../redux/selectors';
+import { selectError, selectFilteredContacts } from './../../redux/selectors';
 
 export const ContactList = () => {
   const dispatch = useDispatch();
-  const contacts = useSelector(selectContacts);
   const filterContacts = useSelector(selectFilteredContacts);
   const error = useSelector(selectError);
 
@@ -26,12 +21,6 @@ export const ContactList = () => {
           <ContactItem contact={contact} key={contact.id} />
         ))}
       </ul>
-      {!error && (
-        <p>
-          You have {contacts.length} contacts. You filtered
-          {filterContacts.length} contacts.
-        </p>
-      )}
     </>
   );
 };
