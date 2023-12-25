e64274e5213bd2b1d8c03fa70ddbd190d16be022
IhorPozhematkin <132217496+IhorPozhematkin@users.noreply.github.com>
2023-12-25 13:31:45 +0200

    goit-react-hw-07-phonebook 25.12-1

--- src/redux/contacts.js
+++ src/redux/contacts.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { addContact, deleteContact, fetchContacts } from './operations';
-import { handlePending, handleRejected } from './helpers';
+import { handlePending, handleFulfilled, handleRejected } from './helpers';
 
 const contactsInitialState = {
   items: [],
@@ -14,23 +14,20 @@ const contactsSlice = createSlice({
   extraReducers: builder => {
     builder
       .addCase(fetchContacts.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.error = null;
         state.items = action.payload;
       })
       .addCase(addContact.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.error = null;
         state.items.unshift(action.payload);
       })
       .addCase(deleteContact.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.error = null;
         state.items = state.items.filter(({ id }) => id !== action.payload.id);
       })
       .addMatcher(action => {
         return action.type.endsWith('pending');
       }, handlePending)
+      .addMatcher(action => {
+        return action.type.endsWith('pending');
+      }, handleFulfilled)
       .addMatcher(action => {
         return action.type.endsWith('rejected');
       }, handleRejected);
